9c2cd617320074f3b013afe5147727f8fc29e47c
Karma63 <adam2004buchan@outlook.com>
2023-12-02 02:55:14 +0000

    made some changed now know that every new register will be in a object called the username so it easier to search for

--- app/src/main/java/com/example/budget/LoginFragment.java
+++ app/src/main/java/com/example/budget/LoginFragment.java
@@ -11,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -32,6 +34,8 @@ import java.util.ArrayList;
  */
 public class LoginFragment extends Fragment implements View.OnClickListener{
 
+    //private static final String LogUsername =RegisterFragment.RegUsername;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -39,8 +43,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mUsername;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -50,16 +53,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param mUsername Parameter 1.
      * @return A new instance of fragment LoginFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
+    public static LoginFragment newInstance(String mUsername) {
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        //args.putString(LogUsername,mUsername);
         fragment.setArguments(args);
         return fragment;
     }
@@ -68,8 +69,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //this.mUsername = getArguments().getString(LogUsername);
         }
     }
 
@@ -83,13 +83,22 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         Button LoginNavHomeButton = view.findViewById(R.id.BtnLogin);
         LoginNavHomeButton.setOnClickListener(this);
+        Toast.makeText(getContext(), "Username"+this.mUsername, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View view) {
         //going to the home fragment if button clicked
         if(view.getId()==R.id.BtnLogin){
+            boolean loginTrue=login(this.getView());
             Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_homeFragment);
         }
     }
+
+    private boolean login(View view){
+        boolean loginTrue=false;
+
+        return loginTrue;
+    }
+
 }
